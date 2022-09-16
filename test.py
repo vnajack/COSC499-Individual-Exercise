89a732acbe0a15f2ae5d226022540ba96513fd02
Veronica <veronica.jack73@gmail.com>
2022-09-15 23:10:26 -0700

    asks user how to sort, added sort by name and score, unit test provided

--- test.py
+++ test.py
@@ -3,10 +3,13 @@ import main
 
 class TestWine(unittest.TestCase):
 
-    test_list = main.make_wine_list(main.wine_names, main.wine_scores)
+    test_names = ["Chardonnay", "Viognier", "Gewurtztraminer", "Pinot Gris", "Riesling"]
+    test_scores = [76, 86, 94, 92, 88]
+    test_list = main.make_wine_list(test_names, test_scores)
     test_instance = test_list[0]
 
-    sorted_list_by_score = []
+    expected_list_by_name = "[0\t76\tChardonnay, 2\t94\tGewurtztraminer, 3\t92\tPinot Gris, 4\t88\tRiesling, 1\t86\tViognier]"
+    expected_list_by_score = "[2\t94\tGewurtztraminer, 3\t92\tPinot Gris, 4\t88\tRiesling, 1\t86\tViognier, 0\t76\tChardonnay]"
 
     # Test if the Wine object's attribute ID is an int
     def test_obj_attr1(self):
@@ -23,6 +26,16 @@ class TestWine(unittest.TestCase):
     # Test if the wine list length is 5
     def test_list_length(self):
         self.assertEqual(len(self.test_list), 5)
+
+    # Test if the sort by name (asc) is correct
+    def test_sort_by_name(self):
+        test_list_by_name = main.sort_by_name(self.test_list)
+        self.assertEqual(self.expected_list_by_name, str(test_list_by_name), "Should be " + self.expected_list_by_name)
+
+    # Test if the sort by score (desc) is correct
+    def test_sort_by_score(self):
+        test_list_by_score = main.sort_by_score(self.test_list)
+        self.assertEqual(self.expected_list_by_score, str(test_list_by_score), "Should be " + self.expected_list_by_score)
 
 if __name__ == "__main__":
     unittest.main()
