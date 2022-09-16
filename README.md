21146fa728c8641d1f166c8c9ed00cde368c8cb7
Veronica <veronica.jack73@gmail.com>
2022-09-15 19:27:34 -0700

    started main and readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# COSC499-Individual-Exercise
+# COSC499 Individual Exercise
+
+## General Information
+
+This individual Git exercise is a small programming assignment to ensure I am familiar with how to use Git by cloning the repository locally, creating and working on branches, and pushing my work appropriately.
+
+A video is included as a short demo to show:
+* my Github account with the repository
+* the program running
+* how to make a simple change locally and push to origin
+* how to make an inline comment in my code
+* how to commit the change and push to origin
+* the results of the successful push
+* the README
+
+## Language and Modules
+
+This application was created with the following language and module versions:
+
+- Python 3.10.7
+
+## Features
---
+++ main.py
@@ -0,0 +1,23 @@
+# Main Python file for the assignment
+
+from operator import attrgetter
+
+# Given 5 wine names and scores, create a Wine object and sort by name and by score
+wine_names = ["Chardonnay", "Viognier", "Gewurtztraminer", "Pinot Gris", "Riesling"]
+wine_scores = [76, 86, 94, 92, 88]
+
+# TODO: create a wine object with attributes ID, name, score
+
+# TODO: create a function to print a wine list
+
+# TODO: create a function to ask the user how they want to sort their wine list
+
+if __name__ == '__main__':
+    print("Congratulations! You now own a winery.")
+    print("All five of your wines have already been tasted and scored by wine critics.")
+    
+    # TODO: present the wine list
+
+    # TODO: ask the user how they want to sort their wine
+
+    # TODO: sort the wine or exit the program
